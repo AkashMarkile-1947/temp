47ae47a5da85a7b989ab376f77e380df0a7d94e5
AkashMarkile-1947 <akashmarkile00@gmail.com>
2023-04-16 11:47:11 +0530

    hero fix

--- components/Hero.js
+++ components/Hero.js
@@ -21,10 +21,10 @@ const HeroSection = () => {
           />
           <div className="absolute top-0 left-[-100px] w-2/3 h-full bg-black opacity-50 z-0 skew-x-12" />
         </div>
-        <div className="absolute top-1/2 left-[33%] transform -translate-x-[50%] -translate-y-[50%] text-left text-white  z-5 h-fit w-[66%] flex flex-col justify-center parent-container">
-        <div className="h-fit w-full bg-emerald-800 skew-x-[165deg] relative left-[-20px] ">
+        <div className="absolute top-1/2 left-[33%] transform -translate-x-[50%] -translate-y-[50%] text-left text-white  z-5 h-fit w-[66%] flex  flex-col justify-center parent-container">
+        <div className="h-fit w-full bg-emerald-800 skew-x-[165deg] relative left-[-20px] hidden md:block ">
           <h1 className="text-xl md:text-[5xl] font-bold mb-[32px] pl-[80px md:20] skew-x-[12deg]">Welcome to my website</h1>
-          <p className="text-base md:text-[2xl] leading-7 w-[45ch] skew-x-[12deg] pl-[80px md:20]">
+          <p className="text-base md:text-[2xl] leading-7 w-[15ch]  md:w-[45ch] skew-x-[12deg] pl-[80px md:20]">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
             tempor incididunt ut labore et dolore magna aliqua.
           </p>
@@ -59,7 +59,7 @@ export const HeroLinks = ({title}) =>  {
           </span>
         </h2>
         <p
-          className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
+          className={`${inter.className} m-0 max-w-[17ch]  md:max-w-[30ch] text-sm opacity-50`}
         >
           Instantly deploy your Next.js site to a shareable URL with Vercel.
         </p>
