18c072c014f09aed9fb2fe34abe861aa2edda297
Akash Smbhaji Markile <110886146+AkashMarkile-1947@users.noreply.github.com>
2023-05-05 01:54:35 +0530

    Update env.js

--- src/pages/api/env.js
+++ src/pages/api/env.js
@@ -2,5 +2,5 @@ export default function handler(req, res) {
     const USER = process.env.USER;
     const PASS  =  process.env.PASS;
     const TO  = process.env.TO;
-    res.status(200).json({ USER, PASS, TO });
+    res.status(200).json({ USER, PASS, TO, name: "hello" });
   }
