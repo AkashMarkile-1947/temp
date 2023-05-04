74d48629ebc8df3b1e73dc5b34e5979d428d8686
Akash Smbhaji Markile <110886146+AkashMarkile-1947@users.noreply.github.com>
2023-05-05 01:41:50 +0530

    Delete env.js

--- src/env.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  const USER = process.env.USER;
-  const PASS  =  process.env.PASS;
-  const TO  = process.env.TO;
-  res.status(200).json({ USER, PASS, TO });
-}
