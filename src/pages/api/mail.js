c0df21f3373922737bd4be3fb5a0c53e8d524d6b
Akash Smbhaji Markile <110886146+AkashMarkile-1947@users.noreply.github.com>
2023-05-06 01:52:53 +0530

    Update mail.js

--- src/pages/api/mail.js
+++ src/pages/api/mail.js
@@ -15,7 +15,6 @@ const  USER =  process.env.USER, PASS =process.env.PASS, TO = process.env.TO;
     }
 });
 
- res.json({USER, PASS, TO, name: "akash"});
 var mailOptions = {
   from: USER,
   to: TO,
